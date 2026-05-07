11ee803a5c7d02aa7cb53813be987b86041da1f6
melvin <melvin.pamplona@gmail.com>
2026-05-07 22:09:58 +0800

    Create canvas for the etch-a-sketch Create new canvas button

--- script.js
+++ script.js
@@ -1,7 +1,14 @@
+function promptNew() {
+	prompt(`Enter new canvas size:`);
+}
+
+const newBtn = document.querySelector('#newBtn');
+newBtn.addEventListener('click', promptNew);
+
 const container = document.querySelector('#container');
 
 for (let i = 0; i < 256; i++) {
 	const div = document.createElement('div');
-	div.textContent = `Div #${i + 1}`;
+	div.classList.add('pixel');
 	container.appendChild(div);
 }
