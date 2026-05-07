b29a5566f0ce10504d4f43304e2af70659b53294
melvin <melvin.pamplona@gmail.com>
2026-05-07 23:27:39 +0800

    Add NEW CANVAS functionality

--- script.js
+++ script.js
@@ -1,14 +1,27 @@
-function promptNew() {
-	prompt(`Enter new canvas size:`);
-}
-
-const newBtn = document.querySelector('#newBtn');
-newBtn.addEventListener('click', promptNew);
+// function promptNew() {
+// 	prompt(`Enter new canvas size:`);
+// }
 
 const container = document.querySelector('#container');
 
-for (let i = 0; i < 256; i++) {
-	const div = document.createElement('div');
-	div.classList.add('pixel');
-	container.appendChild(div);
-}
+const newBtn = document.querySelector('#newBtn');
+newBtn.addEventListener('click', () => {
+	let squarePerSide = parseInt(prompt('Enter new canvas size:'));
+	if (isNaN(squarePerSide) || squarePerSide <= 0 || squarePerSide > 100) {
+		return;
+	}
+
+	let totalSide = squarePerSide * squarePerSide;
+
+	container.innerHTML = '';
+
+	for (let i = 0; i < totalSide; i++) {
+		const div = document.createElement('div');
+		div.classList.add('pixel');
+		div.style.width = `${100 / squarePerSide}%`;
+		div.style.height = `${100 / squarePerSide}%`;
+		container.appendChild(div);
+	}
+});
+
+// 100 / number of squares per side
